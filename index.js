9b879ffed3f88bc5e0c8e7449f64ccf6480d6937
mpawas <pawas.mis@gmail.com>
2023-11-09 01:54:20 +0545

    added.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const port = 3000;
-const htmlParser = require('node-html-parser');
 const axios = require('axios');
 const cheerio = require('cheerio')
 const admin = require('firebase-admin');
@@ -14,11 +13,9 @@ const filePath = 'newToken.txt';
 const password = 'Exper!ence4@ll';
 const url = 'https://www.drikpanchang.com/?geoname-id=1282988&&date=2023/11/08';
 const timeSchedule = '0 8-20/3 * * *';
-const testschedule = '* * * * *'
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
-
 
 const firebaseAdmin = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -32,7 +29,7 @@ const sendPushNotification = async (deviceToken, title, body) => {
 }
 
 app.post('/', (req, res) => {
-    if (password !== req.body.password) return res.status(404).json({ message: "Password Not Matched." })
+    console.log('api called for the post request');
     fs.writeFile(filePath, req.body.newToken, (err) => {
         if (err) {
             res.status(400).json({ message: "error" })
@@ -40,6 +37,7 @@ app.post('/', (req, res) => {
     });
     res.status(200).json({ message: "Updated." })
 })
+
 
 const apiCall = () => {
     axios.get(url).then((response) => {
@@ -51,14 +49,22 @@ const apiCall = () => {
                 lunarMonth: $('.dpPHeaderLeftContent div:nth-child(2) div:nth-child(2)').text().trim(),
                 tithi2: $('.dpSectionBlockLinks span:nth-child(12)').text().trim(),
             };
+            fs.readFile(filePath, 'utf8', (err, data) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    try {
+                        sendPushNotification(data, "Pawas Calendar Update", ` Today's ${info.lunarMonth} \n ${info.tithi2}`);
+                    } catch (err) {
 
-            console.log(info.lunarMonth);
-            console.log(info.tithi2);
+                    }
+                }
+            });
         }
     })
 }
 
-const task = cron.schedule(testschedule, () => {
+const task = cron.schedule('*/5 * * * *', () => {
     apiCall();
 }, {
     scheduled: false
